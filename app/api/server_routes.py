6f460c9d4c3830a9583c5276b16ebada8d963740
Kevin <kevintvu123@gmail.com>
2022-12-10 14:17:31 -0500

    updated server_routes

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_login import login_required, current_user
-from app.models import User
+from app.models import db, User, Server
 
 server_routes = Blueprint("servers", __name__)
 
@@ -13,12 +13,29 @@ def get_servers_current():
     """
     # print('This is this person id', current_user.id)
     user = User.query.get(current_user.id)
-    print("This is the user", user.to_dict())
+    # print("This is the user", user.to_dict())
 
     servers = user.servers
-    print("This is the servers", servers)
+    # print("This is the servers", servers)
 
-    return "success"
+    return
 
 
+@server_routes.route("/<int:server_id>/users", methods=["POST"])
+@login_required
+def post_server_member(server_id):
+    """
+    Add a user to a specified server id
+    """
+    user = User.query.get(current_user.id)
+    user_id = user.to_dict()["id"]
+    # print(id)
 
+    server = Server.query.get(server_id)
+    server_id = server.to_dict()["id"]
+    # print(type(server.members))
+
+    server.members.append(user)
+    db.session.commit()
+
+    return
