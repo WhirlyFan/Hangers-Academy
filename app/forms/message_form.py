31822abedd8e9956d607ce138e3fa102de24ffac
Michael Lee <mhykol2114@gmail.com>
2022-12-10 15:22:04 -0800

    wip
    Co-authored-by: Kevin Vu <kevintvu123@users.noreply.github.com>

--- app/forms/message_form.py
+++ app/forms/message_form.py
@@ -1,1 +1,13 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, ValidationError
+
+
+def message_check(form,field):
+    #Check message content length
+    message_content = field.data
+    if len(message_content) > 255:
+        raise ValidationError("")
+
+class CreateMessage(FlaskForm):
+    message_content = StringField("Message Content", validators=[DataRequired()])
