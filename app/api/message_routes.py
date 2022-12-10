b9b9d9668001bde0f803a0f888630d49fcd8aad4
Michael Lee <mhykol2114@gmail.com>
2022-12-10 14:40:08 -0800

    live share test?! plus websocket stuff
    Co-authored-by: Kevin Vu <kevintvu123@users.noreply.github.com>

--- app/api/message_routes.py
+++ app/api/message_routes.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, request
 from flask_login import login_required, current_user
 # from ..forms.server_form import CreateServer, UpdateServer
-# from app.models import db, User, Server
+from app.models import db, Message
 
 message_routes = Blueprint("messages", __name__)
 
-# @message_routes.route("")
+@message_routes.route("/messages")
+@login_required
+def create_message():
+    pass
