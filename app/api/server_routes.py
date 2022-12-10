6e0dcf95ebb73f235145da4c2a870eb888e9427d
Kevin <kevintvu123@gmail.com>
2022-12-10 16:00:36 -0500

    partial server_route PUT

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_login import login_required, current_user
+from ..forms.server_form import CreateServer, UpdateServer
 from app.models import db, User, Server
 
 server_routes = Blueprint("servers", __name__)
@@ -24,7 +25,9 @@ def get_server_details(server_id):
     """
     Query for details of a server by server id
     """
-    
+    server = Server.query.get(server_id).to_dict()
+
+    return server
 
 
 @server_routes.route("/")
@@ -54,3 +57,50 @@ def post_server_member(server_id):
     db.session.commit()
 
     return server.to_dict()
+
+
+@server_routes.route("", methods=["POST"])
+@login_required
+def create_server():
+    """
+    Create a new server
+    """
+    form = CreateServer()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    if form.validate_on_submit():
+        data = form.data
+        create_server = Server(
+            name=data["name"],
+            server_img=data["server_img"],
+            private=data["private"],
+            owner_id=current_user.id,
+        )
+
+        user = User.query.get(current_user.id)
+        create_server.members.append(user)
+
+        db.session.add(create_server)
+        db.session.commit()
+
+        return create_server.to_dict()
+
+    return "Error Response Here"
+
+
+@server_routes.route("/<int:server_id>/edit", methods=["PUT"])
+@login_required
+def update_server(server_id):
+    form = UpdateServer()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    server = Server.query.get(server_id)
+
+    if server and form.validate_on_submit():
+        data = form.data
+        server.name = (data["name"],)
+        server.server_img = (data["server_img"],)
+
+        db.session.commit()
+        return
+    return "Unsuccessful Response"
