0770a37a55e587d2f72c1897c797af2e0c0d14dd
Kevin <kevintvu123@gmail.com>
2022-12-10 19:52:47 -0500

    Post Message Route

--- app/api/message_routes.py
+++ app/api/message_routes.py
@@ -1,11 +1,32 @@
 from flask import Blueprint, request
 from flask_login import login_required, current_user
-# from ..forms.server_form import CreateServer, UpdateServer
+from ..forms.message_form import CreateMessage
 from app.models import db, Message
+from .auth_routes import validation_errors_to_error_messages
 
 message_routes = Blueprint("messages", __name__)
 
-@message_routes.route("/messages")
+
+@message_routes.route("", methods=["POST"])
 @login_required
-def create_message():
-    pass
+def new_message():
+    """
+    Creates a message for a channel with channel id in body request
+    """
+
+    form = CreateMessage()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    if form.validate_on_submit():
+        data = form.data
+        new_message = Message(
+            message_content=data["message_content"],
+            channel_id=data["channel_id"],
+            user_id=current_user.id,
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        return new_message.to_dict()
+    return {"errors": validation_errors_to_error_messages(form.errors)}, 401
--- app/forms/message_form.py
+++ app/forms/message_form.py
@@ -1,13 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, ValidationError
+from app.models import Channel
 
 
-def message_check(form,field):
-    #Check message content length
+def message_check(form, field):
+    # Check message content length
     message_content = field.data
     if len(message_content) > 255:
-        raise ValidationError("")
+        raise ValidationError("Message must be less than 255 characters")
+
+
+def channel_check(form, field):
+    # Check if channel exists
+    channel_id = field.data
+    channel = Channel.query.get(int(channel_id))
+    if not channel:
+        raise ValidationError("Channel couldn't be found")
+
 
 class CreateMessage(FlaskForm):
-    message_content = StringField("Message Content", validators=[DataRequired()])
+    message_content = StringField(
+        "Message Content", validators=[DataRequired(), message_check]
+    )
+    channel_id = IntegerField("Channel_id", validators=[DataRequired(), channel_check])
