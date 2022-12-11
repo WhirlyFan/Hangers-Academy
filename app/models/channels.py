89d9bff9a114a062c81901716427433478113286
Kevin <kevintvu123@gmail.com>
2022-12-11 14:55:18 -0500

    bug fixed

--- app/models/channels.py
+++ app/models/channels.py
@@ -5,12 +5,13 @@ class Channel(db.Model):
     __tablename__ = "channels"
 
     if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
+        __table_args__ = {"schema": SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    server_id = db.Column(db.Integer, db.ForeignKey(
-        add_prefix_for_prod("servers.id")), nullable=False)
-    name = db.Column(db.String(30), default='general')
+    server_id = db.Column(
+        db.Integer, db.ForeignKey(add_prefix_for_prod("servers.id")), nullable=False
+    )
+    name = db.Column(db.String(30), default="general")
 
     @property
     def _name(self):
@@ -21,13 +22,15 @@ class Channel(db.Model):
         self.name = name
 
     server = db.relationship("Server", back_populates="channels")
-    messages = db.relationship("Message", cascade="all, delete-orphan", back_populates="channel")
-    
+    messages = db.relationship(
+        "Message", cascade="all, delete-orphan", back_populates="channel"
+    )
+
     def to_dict(self):
         return {
-            'id': self.id,
-            'server_id': self.server_id,
-            'name': self.name,
-            "Server": self.server.to_dict(),
+            "id": self.id,
+            "server_id": self.server_id,
+            "name": self.name,
+            # "Server": self.server.to_dict(),
             # "Messages": [message.to_dict() for message in self.messages]
         }
