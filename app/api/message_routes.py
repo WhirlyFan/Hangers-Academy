f173aab4a343ea0aa478327c9b23ac2c4bd682a4
Michael Lee <mhykol2114@gmail.com>
2022-12-10 17:27:15 -0800

    message routes except get all messages by channel
    Co-authored-by: Kevin Vu <kevintvu123@users.noreply.github.com>

--- app/api/message_routes.py
+++ app/api/message_routes.py
@@ -3,7 +3,7 @@ from flask_login import login_required, current_user
 from ..forms.message_form import CreateMessage, UpdateMessage
 from app.models import db, Message
 from .auth_routes import validation_errors_to_error_messages
-
+# add authorized from auth_routes
 message_routes = Blueprint("messages", __name__)
 
 
@@ -38,10 +38,36 @@ def update_message(message_id):
     """
     Updates a message
     """
-
     form = UpdateMessage()
     form["csrf_token"].data = request.cookies["csrf_token"]
 
     message = Message.query.get(message_id)
 
-    return "Success"
+    if message and form.validate_on_submit():
+        data = form.data
+        message.message_content = data["message_content"]
+        db.session.commit()
+        return message.to_dict()
+
+    return {"errors": validation_errors_to_error_messages(form.errors)}, 401
+
+@message_routes.route("/<int:message_id>", methods=["DELETE"])
+@login_required
+def delete_message(message_id):
+    """
+    Deletes a message
+    """
+    message = Message.query.get(message_id)
+
+    if not message:
+        return {
+            "message": "Message couldn't be found",
+            "status_code": 404
+        }
+    else:
+        db.session.delete(message)
+        db.session.commit()
+        return {
+            "message": "Sucessfully deleted",
+            "status_code": 200
+        }
