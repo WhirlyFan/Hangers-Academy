5683849ac02c2f6cc7f1f297c6a8093d84a24aa6
Michael Lee <mhykol2114@gmail.com>
2022-12-13 12:53:39 -0800

    landing-page setup

--- react-app/src/components/Landing.js
+++ react-app/src/components/Landing.js
@@ -1,7 +1,11 @@
-
+import landing_background from "../assets/landing_background.svg";
+import styles from "./cssModules/Landing.module.css";
 
 export default function Landing() {
-    return (
-        <div></div>
-    )
-};
+  return (
+    <div className={styles.landing_background}>
+      <span>landing</span>
+      <img src={landing_background} alt="landing_background"></img>
+    </div>
+  );
+}
--- react-app/src/components/view/FriendsView.js
+++ react-app/src/components/view/FriendsView.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import styles from "../css-modules/FriendsView.module.css"
-
 export default function FriendsView() {
     return (
         <div></div>
