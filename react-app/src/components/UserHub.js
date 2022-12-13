f592c76c50a656338e0b4b42c84397fc7eec05a0
preston <prestonprince18@gmail.com>
2022-12-13 13:52:23 -0700

    CSS MAINNNNN Co-authored-by: Linus Huynh <linushuynh@users.noreply.github.com> Co-authored-by: Kevin Vu <kevintvu123@users.noreply.github.com>

--- react-app/src/components/UserHub.js
+++ react-app/src/components/UserHub.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function UserHub() {
     return (
-        <div></div>
+        <div>USER HUB</div>
     )
 };
--- react-app/src/components/view/ServersView.js
+++ react-app/src/components/view/ServersView.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function ServersView() {
     return (
-        <div></div>
+        <div>ServersView</div>
     )
 };
