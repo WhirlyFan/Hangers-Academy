9dc655ffe0e7f807bb1abc2fe522720ac17cbe4e
Michael Lee <mhykol2114@gmail.com>
2022-12-13 16:13:59 -0800

    landing setup

--- react-app/src/components/Landing.js
+++ react-app/src/components/Landing.js
@@ -1,11 +1,18 @@
 import landing_background from "../assets/landing_background.svg";
+import landing_right from "../assets/landing_right.svg";
+import landing_left from "../assets/landing_left.svg";
 import styles from "./cssModules/Landing.module.css";
 
 export default function Landing() {
   return (
     <div className={styles.landing_background}>
-      <span>landing</span>
-      <img src={landing_background} alt="landing_background"></img>
+      <div>
+        <img src={landing_background} alt="landing_background"></img>
+      </div>
+      <div className={styles.landing_people}>
+        <img src={landing_left} alt="landing_left"></img>
+        <img src={landing_right} alt="landing-right"></img>
+      </div>
     </div>
   );
 }
