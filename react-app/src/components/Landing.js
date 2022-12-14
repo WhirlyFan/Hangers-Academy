77fbe08ca4f0d5bd7b7d81aa0fb77e8e12af498e
Michael Lee <mhykol2114@gmail.com>
2022-12-14 00:15:16 -0800

    css and login button

--- react-app/src/components/Landing.js
+++ react-app/src/components/Landing.js
@@ -1,17 +1,47 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
 import landing_background from "../assets/landing_background.svg";
 import landing_right from "../assets/landing_right.svg";
 import landing_left from "../assets/landing_left.svg";
 import styles from "./cssModules/Landing.module.css";
 
+//fix background layout
 export default function Landing() {
+  const history = useHistory();
+
+  const openDiscord = (e) => {
+    e.preventDefault();
+    history.push(`/login`);
+  };
+
   return (
-    <div className={styles.landing_background}>
+    <div className={styles.background}>
+      <img
+        className={styles.background_image}
+        src={landing_background}
+        alt="landing_background"
+      ></img>
+      <img
+        className={styles.left_image}
+        src={landing_left}
+        alt="landing_left"
+      ></img>
+      <img
+        className={styles.right_image}
+        src={landing_right}
+        alt="landing-right"
+      ></img>
       <div>
-        <img src={landing_background} alt="landing_background"></img>
-      </div>
-      <div className={styles.landing_people}>
-        <img src={landing_left} alt="landing_left"></img>
-        <img src={landing_right} alt="landing-right"></img>
+        {/* <h1>IMAGINE A PLACE...</h1>
+        <p>
+          ...where you can belong to a school club, a gaming group, or a
+          worldwide art community. Where just you and a handful of friends can
+          spend time together. A place that makes it easy to talk every day and
+          hang out more often.
+        </p> */}
+        <button className={styles.login_button} onClick={openDiscord}>
+          Open Discord in your browser
+        </button>
       </div>
     </div>
   );
