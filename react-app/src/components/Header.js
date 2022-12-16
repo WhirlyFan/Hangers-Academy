b3128daf4af2d9fa22279cce6c7a0c4fc186eb8b
Kevin <kevintvu123@gmail.com>
2022-12-16 06:11:27 -0500

    completed headers

--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -1,6 +1,80 @@
+import { useLocation } from "react-router-dom"
+import styles from "./cssModules/Header.module.css"
+import friend_btn from "./../assets/friend_btn.png"
+import { useSelector } from "react-redux"
 export default function Header() {
+    const location = useLocation()
+    const locationPathArr = (location.pathname).split('/')
+
+    const user = useSelector(state => state.session.user)
+
+    let serverChannelName
+    let otherMember
+
+    if (locationPathArr.length === 5) {
+        const publicServers = user.public_servers
+
+        for (let server of publicServers) {
+            for (let channel of server.Channels) {
+                if (channel.id === parseInt(locationPathArr[4])) {
+                    serverChannelName = channel.name
+                }
+            }
+        }
+    }
+
+    if (locationPathArr.length === 6) {
+        const privateServers = user.private_servers
+
+        for (let server of privateServers) {
+            if (server.id === parseInt(locationPathArr[4]))
+                otherMember = (server.name.split("_")[1])
+        }
+
+    }
 
     return (
-        <div>HEADER</div>
+        <div className={styles.bigContainer}>
+            {(locationPathArr[2] === "friends") && (
+                <div className={styles.headerContainer}>
+                    <div className={styles.friendButtonContainer}>
+                        <img
+                            className={styles.friend}
+                            src={friend_btn}
+                            alt='friendbtn'
+                        >
+                        </img>
+                    </div>
+                    <div>
+                        Friends
+                    </div>
+                </div>
+            )
+            }
+            {(locationPathArr.length === 5) && (
+                <div className={styles.headerContainer}>
+                    <div className={styles.friendButtonContainer}>
+                        <span class="material-symbols-outlined">
+                            tag
+                        </span>
+                    </div>
+                    <div>
+                        {serverChannelName}
+                    </div>
+                </div>
+            )}
+            {(locationPathArr.length === 6) && (
+                <div className={styles.headerContainer}>
+                    <div className={styles.friendButtonContainer}>
+                        <span class="material-symbols-outlined">
+                            alternate_email
+                        </span>
+                    </div>
+                    <div>
+                        {otherMember}
+                    </div>
+                </div>
+            )}
+        </div >
     )
 };
--- react-app/src/components/list/MemberList.js
+++ react-app/src/components/list/MemberList.js
@@ -1,38 +1,38 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import styles from "../cssModules/UsersView.module.css"
 
 export default function MemberList() {
-    const allServers = useSelector(state => state.server.allServers)
-    const {serverId} = useParams()
+  const allServers = useSelector(state => state.server.allServers)
+  const { serverId } = useParams()
 
-    const members = allServers[+serverId].Members
-    
-    return (
+  const members = allServers[+serverId].Members
+
+  return (
     <div className={styles.container}>
-      <span className={styles.add_friends}>Add Friends</span>
-        <div className={styles.users_list}>
-          {members.map((user) => {
-            return (
-              <div key={user.id} className={styles.user_card}>
-                <div className={styles.left}>
-                  <div className={styles.icon_container}>
-                    <img
+      <span className={styles.add_friends}>Members</span>
+      <div className={styles.users_list}>
+        {members.map((user) => {
+          return (
+            <div key={user.id} className={styles.user_card}>
+              <div className={styles.left}>
+                <div className={styles.icon_container}>
+                  <img
                     className={styles.icon}
                     src="https://cdn.discordapp.com/attachments/1049445170778738789/1051654101286527137/1.png"
                     alt="default-icon"
-                    >
-                    </img>
-                  </div>
-                  <div>{user.username}</div>
+                  >
+                  </img>
                 </div>
+                <div>{user.username}</div>
               </div>
-            )
-          })}
-        </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
     // <div></div>
-    )
+  )
 };
